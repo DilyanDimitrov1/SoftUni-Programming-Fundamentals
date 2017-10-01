b4045646005abc76233cdc43ffac5f35e89926ee
dilqncho <dilyandimitrov8@gmail.com>
2017-10-02 02:18:48 +0300

    Data Types and Variables lab practice

---
+++ DataTypesAndVariablesLab/RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            bool magicNum = false;
+            int sum = 0;
+            for (int x = 1; x <= n; x++)
+            {
+                while (x > 0)
+                {
+                     sum += x % 10;
+                     x = x / 10;
+                }//while
+                magicNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{x} -> {magicNum}");
+            }//for
+
+        }
+    }
+}
---
+++ DataTypesAndVariablesLab/SpecialNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int x = 1; x <= n; x++)
+            {
+                int lastDigit = x % 10;
+                int beforeDigits = x / 10;
+                sum = lastDigit + beforeDigits;
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{x} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{x} -> False");
+                }
+                    
+
+            }
+        }
+    }
+
+}
